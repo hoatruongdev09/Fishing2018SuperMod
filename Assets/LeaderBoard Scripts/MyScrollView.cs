12abcea50b4a9931fe1671b2072495e39e725428
hoatruongdev09 <hoatruongdev09@gmail.com>
2018-07-28 09:53:35 +0700

    fuck
    sdf

--- Assets/LeaderBoard Scripts/MyScrollView.cs
+++ Assets/LeaderBoard Scripts/MyScrollView.cs
@@ -13,39 +13,39 @@ public class MyScrollView : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     private bool isDragging;
 
     private Vector3 pointerPosition;
-    public Vector3 deltaPosition;
-    public Vector3 lastPosition;
+    private Vector3 distanceToPointer;
 
     private void Start() {
         backGroundImage = GetComponent<Image>();
-        lastPosition = content.rectTransform.position;
-        //deltaPosition = content.rectTransform.position - lastPosition;
+
     }
 
     private void Update() {
         if (isDragging) {
             if (horizontal) {
-                content.rectTransform.position = Vector3.Lerp(content.rectTransform.position, new Vector3(pointerPosition.x, content.rectTransform.position.y) + deltaPosition, smoothSpeed * Time.deltaTime);
+                content.rectTransform.position = Vector3.Lerp(content.rectTransform.position, new Vector3(pointerPosition.x - distanceToPointer.x, content.rectTransform.position.y), smoothSpeed * Time.deltaTime);
             }
             if (vertical) {
-                content.rectTransform.position = Vector3.Lerp(content.rectTransform.position, new Vector3(content.rectTransform.position.x, pointerPosition.y) + deltaPosition, smoothSpeed * Time.deltaTime);
+                content.rectTransform.position = Vector3.Lerp(content.rectTransform.position, new Vector3(content.rectTransform.position.x, pointerPosition.y - distanceToPointer.y), smoothSpeed * Time.deltaTime);
             }
         }
     }
     public void OnDrag(PointerEventData eventData) {
-        isDragging = true;
-        pointerPosition = eventData.position;
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(backGroundImage.rectTransform, eventData.position, eventData.pressEventCamera, out pos)) {
+            isDragging = true;
+            pointerPosition = eventData.position;
+        }
 
     }
 
     public void OnPointerDown(PointerEventData eventData) {
-
+        distanceToPointer = (Vector3)eventData.position - content.rectTransform.position;
     }
 
     public void OnPointerUp(PointerEventData eventData) {
         isDragging = false;
-        deltaPosition = content.rectTransform.position - lastPosition;
-        lastPosition = content.rectTransform.position;
+
         //throw new System.NotImplementedException();
     }
 }
